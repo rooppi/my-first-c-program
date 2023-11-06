acf33628821941f1834a7e3535110a37804692ba
rooppi <prostoxer@icloud.com>
2023-11-06 22:10:04 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿while (true)
+{
+    Console.WriteLine("Що саме ви хочете зробити:\n віднімання '-' чи додавання '+'\n або ділення '/' або '*' ");
+
+    string symbol = Console.ReadLine();
+
+    if (symbol == "+")
+    {
+        Console.WriteLine("Введи перше число");
+
+
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        //Конвертируем число ля возмодности использования ред лайна 
+
+        Console.WriteLine("Введи друге число");
+
+
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        // такойже принцып как и в первом
+
+
+        Console.WriteLine("Ответ:" + (number1 + number2));
+    }
+    else if (symbol == "-")
+    {
+        Console.WriteLine("Введи перше число");
+
+
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        //Конвертируем число ля возмодности использования ред лайна
+
+
+        Console.WriteLine("Введи друге число");
+
+
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        // такойже принцып как и в первом
+
+
+        Console.WriteLine("Ответ:" + (number1 - number2));
+    }
+
+    else if (symbol == "*")
+    {
+        Console.WriteLine("Введи перше число");
+
+
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        //Конвертируем число ля возмодности использования ред лайна 
+
+        Console.WriteLine("Введи друге число");
+
+
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        // такойже принцып как и в первом
+
+
+        Console.WriteLine("Ответ:" + (number1 * number2));
+    }
+    else if (symbol == "/")
+    {
+        Console.WriteLine("Введи перше число");
+
+
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        //Конвертируем число ля возмодности использования ред лайна 
+
+        Console.WriteLine("Введи друге число");
+
+
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        // такойже принцып как и в первом
+
+
+        Console.WriteLine("Ответ:" + (number1 / number2));
+    }
+
+}
+
